cd788f5f1b4e3b23dabaaa87ecf6c7753eec28dd
momagdy405-cmyk <momagdy405@gmail.com>
2026-08-29 03:44:52 +0300

    Add project files.

---
+++ ass2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+
+namespace ass2
+{
+    internal class Program
+    {
+        
+        //static void printfirstbook(string[]books)
+        //{
+          
+        //    if (books == null)
+        //    {
+        //        return;
+        //    }
+        //    Console.WriteLine(books[0]);
+        //}
+        static void Main(string[] args)
+        {
+            //string title = "clean code";
+            //string uppertitle = title.ToUpper();
+            //Console.Write($"{title} {uppertitle}");
+            //string s01 = "clean code";
+            //string s02 = "clean code";
+            //Console.WriteLine(ReferenceEquals(s01, s02));
+            //StringBuilder s01 = new StringBuilder();
+            //s01.Append("booklist");
+            //s01.Append("-updated");
+            //Console.Write(s01);
+            //s01.Replace("booklist", "library");
+            //Console.Write(s01);
+            //string title = "clean code";
+            //int pages = 464;
+            //Console.Write("Book:"+title+pages);
+            //Console.WriteLine($"Book:{title}{pages}");
+            //Console.WriteLine(string.Format("Book:{0},{1}",title,pages));
+            //int pages = 464;
+            //if (pages > 300)
+            //{
+            //    Console.WriteLine("largebook");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("smallbook");
+            //}
+            //int pages = 464;
+            //bool isavaliable = true;
+            //if (pages > 300&&isavaliable)
+            //{
+            //    Console.WriteLine("you can borrow this book");
+            //}
+            //string title = "refactoring";
+            //switch (title)
+            //{
+            //    case "clean code": Console.WriteLine("great job");break;
+            //    case "refactoring": Console.WriteLine("nice pick"); break;
+            //    default:Console.WriteLine("never heard of it");break;            
+            //}
+            //int pages = 464;
+            //Console.WriteLine(pages > 300 ? "large book" : "short book");
+            //string[] books = { "clean code", "pragmatic programming", "refactoring" };
+            //printfirstbook(books);
+            ////for(int i = 0; i < books.Length; i++)
+            ////{
+            ////    Console.WriteLine($"{i+1}.{books[i]}");
+            ////}
+            //int i = 0;
+            //while (i < books.Length)
+            //{
+            //    Console.WriteLine($"{i + 1}.{books[i]}");
+            //    i += 1;
+
+            //}
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine("checkingbooks");
+            //    i += 1;
+            //} while (i < 3);
+            //int i= 0;
+            //foreach(string book in books)
+            //{
+            //    Console.WriteLine($"{i+1}.{books[i]}");
+            //    i += 1;
+            //}
+            //int i= 0;
+            //foreach(string book in books)
+            //{
+            //    Console.WriteLine(books[i]);
+            //    i += 1;
+            //    if (book == "refactoring")
+            //    {
+            //        break;
+            //    }
+            //}
+            //for (int i = 0; i < books.Length; i++)
+            //{
+            //    if (books[i]=="pragmatic programming")
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(books[i]);
+            //}
+
+
+
+            
+
+
+
+
+
+        }
+    }
+}

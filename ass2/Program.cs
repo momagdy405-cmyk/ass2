41a96b785008fb7cecc07c47277ba40fc3b9a9ac
momagdy405-cmyk <momagdy405@gmail.com>
2026-08-29 04:26:46 +0300

    q9

--- ass2/Program.cs
+++ ass2/Program.cs
@@ -62,6 +62,15 @@ namespace ass2
             //    Console.WriteLine("smallbook");
             //}
             #endregion
+            #region Q9
+            // Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book" only if pages is greater than 300 and isAvailable is true.Use the && operator. 
+            //int pages = 464;
+            //bool isavaliable = true;
+            //if (pages > 300&&isavaliable)
+            //{
+            //    Console.WriteLine("you can borrow this book");
+            //}
+            #endregion
         }
     }
 }
